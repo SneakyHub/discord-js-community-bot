c238708f02100fc133429ad23e152d7b348cddcb
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-04-01 10:55:57 -0400

    Add files via upload

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,18 +4,29 @@ module.exports = {
     name: "help",
     aliases: [],
     run: async (client, message, args) => {
-        let CommandsArray = [];
+        if (!args[0]) {
+            let CommandsArray = [];
 
-        client.commands.forEach(cmd => {
-            if (CommandsArray.includes(cmd.name)) return;
-            CommandsArray.push(cmd.name)
-        });
+            client.commands.forEach(cmd => {
+                if (CommandsArray.includes(cmd.name)) return;
+                CommandsArray.push(cmd.name)
+            });
 
-        const embed = new Discord.MessageEmbed()
-            .setAuthor(message.author.tag)
-            .setTitle("Help Menu")
-            .addField("Command List", `${CommandsArray.toString()}`)
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(message.author.tag)
+                .setTitle("Help Menu")
+                .addField("Command List", `${CommandsArray.toString()}`)
 
-        message.channel.send(embed);
+            message.channel.send(embed);
+        } else {
+            let cmd = client.commands.get(args[0]);
+
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(message.author.tag)
+                .setTitle("Command Help Menu")
+                .addField("Aliases", `${cmd.aliases || "None"}`)
+
+            message.channel.send(embed);
+        }
     }
 }
