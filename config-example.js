f9dbd4c0a9b60bf4ec6bfbfb0f83cb2ca23b0014
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-04-05 18:00:45 -0400

    Add files via upload

--- config-example.js
+++ config-example.js
@@ -2,5 +2,7 @@ module.exports = {
     // Discord bot
     // rename this file to "config.js"
     token: "your token here",
-    prefix: "prefix here"
+    prefix: "prefix here",
+    eval_ids: [],
+    logs_channel: ""
 }
