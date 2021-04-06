c045eecd1bc1eec637cf5f3c476927f21969401e
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-04-06 11:56:04 -0400

    Add files via upload
    fix bot-info.js

--- src/commands/bot-info.js
+++ src/commands/bot-info.js
@@ -6,77 +6,76 @@ require("moment-duration-format");
 module.exports = {
   name: "bot-info",
   aliases: ["info"],
-  run: async (client, msg, args) => {
+  run: async (client, message, args) => {
     // code here
     var msg = await message.channel.send("Getting Status...");
 
-  var osType = await os.type();
+    var osType = await os.type();
 
-  if (osType === "Darwin") osType = "macOS";
-  else if (osType === "Windows") osType = "Windows";
-  else osType = os.type();
+    if (osType === "Darwin") osType = "macOS";
+    else if (osType === "Windows") osType = "Windows";
+    else osType = os.type();
 
-  var embed = {
-    color: 3447003,
-    description: "**Status**",
-    fields: [
-      {
-        name: "❯ Bot Owner",
-        value: client.users.cache.get("716761186812821604"),
-        inline: false
-      },
-      {
-        name: "❯ Uptime",
-        value: moment
-          .duration(client.uptime)
-          .format("D [Day], H [Hour], m [Minutes], s [Seconds]"),
-        inline: false
-      },
-      {
-        name: "❯ Server OS",
-        value: `${osType}`,
-        inline: false
-      },
-      {
-        name: "❯ Memory",
-        value: `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`,
-        inline: false
-      },
-      {
-        name: "❯ CPU Model",
-        value: `• ${os.cpus().map(i => `${i.model}`)[0]} `,
-        inline: false
-      },
-      {
-        name: "❯ CPU Usage",
-        value: `• ${Math.round(require("os").loadavg()[0] * 100) / 100}%`,
-        inline: false
-      },
-      {
-        name: " ❯ General İnformations",
-        value: `             
+    var embed = {
+      color: 3447003,
+      description: "**Status**",
+      fields: [
+        {
+          name: "❯ Bot Owner",
+          value: client.users.cache.get("716761186812821604"),
+          inline: false
+        },
+        {
+          name: "❯ Uptime",
+          value: moment
+            .duration(client.uptime)
+            .format("D [Day], H [Hour], m [Minutes], s [Seconds]"),
+          inline: false
+        },
+        {
+          name: "❯ Server OS",
+          value: `${osType}`,
+          inline: false
+        },
+        {
+          name: "❯ Memory",
+          value: `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`,
+          inline: false
+        },
+        {
+          name: "❯ CPU Model",
+          value: `• ${os.cpus().map(i => `${i.model}`)[0]} `,
+          inline: false
+        },
+        {
+          name: "❯ CPU Usage",
+          value: `• ${Math.round(require("os").loadavg()[0] * 100) / 100}%`,
+          inline: false
+        },
+        {
+          name: " ❯ General İnformations",
+          value: `             
 • Server: ${client.guilds.cache.size}                   
 • Channel: ${client.channels.cache.size}                   
 • User: ${client.guilds.cache
-            .reduce((a, b) => a + b.memberCount, 0)
-            .toLocaleString()}
+              .reduce((a, b) => a + b.memberCount, 0)
+              .toLocaleString()}
                     `,
-        inline: false
-      },
+          inline: false
+        },
 
-      {
-        name: "❯ Versions",
-        value: `
+        {
+          name: "❯ Versions",
+          value: `
            • Discord.js: v${Discord.version}
            • Node: ${process.version}
                     `,
-        inline: false
-      }
-    ],
-    thumbnail: { url: client.user.displayAvatarURL() }
-  };
+          inline: false
+        }
+      ],
+      thumbnail: { url: client.user.displayAvatarURL() }
+    };
 
-  return message.edit("", { embed });
-};
+    return message.edit("", { embed });
   }
 }
