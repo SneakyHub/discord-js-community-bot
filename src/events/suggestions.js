4302757d30cbe550d7df65766377ddcbaaf95256
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-04-07 08:40:19 -0400

    Add files via upload

--- src/events/suggestions.js
+++ src/events/suggestions.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports = {
     name: "message",
     run: async (client, message, args) => {
-        if (!message.channel.id == "826165658541752390") return;
+        if (message.channel.id != "826165658541752390") return;
         if (message.author.bot) return;
 
         const { content, author, createdTimestamp } = message;
