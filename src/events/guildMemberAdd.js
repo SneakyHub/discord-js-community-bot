e686d317f7e25c3d4402fd950de51da350a651ab
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-04-13 06:15:41 -0400

    Add files via upload
    fix join system

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 module.exports = {
     name: "guildMemberAdd",
     run: async (client, member) => {
@@ -5,5 +7,19 @@ module.exports = {
         let channel = member.guild.channels.cache.find(c => c.id == `${channel_id}`);
 
         channel.send(`${member} welcome to ${member.guild.name}, head over to <#825797019103789056> to get started with hosting! If you need help feel free to ask for it in <#828253375387795486>!`);
+
+        if (Date.now() - member.user.createdAt < 259200000) {
+            member.ban({ reason: "Account is less than 3 days old. - Banned by Auto-Ban" });
+            channel.send(`${member.user.tag} was banned due to the account being less than 3 days old.`);
+        } else if (Date.now() - member.user.createdAt < 2419200000) {
+            try {
+                member.user.send(`Sorry! We only allow accounts over the age of 28 days to join.\nYour account was created ${hd(Date.now() - member.user.createdAt, { round: true })} ago.\n\nYou are welcome to join again once this account is over 28 days old!`);
+            } catch (error) {
+                return;
+            }
+
+            member.kick(`Account is less than 28 days old. - Kicked by Auto-Kick`);
+            channel.send(`${member.user.tag} was kicked due to the account being less than 10 days old.`);
+        }
     }
 }
