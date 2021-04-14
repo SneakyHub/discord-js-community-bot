3079cbdf68455a216f15248136ee4643f38829da
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-04-14 18:36:41 -0400

    remove weird characters, change `var` to `let`
    fix other stuff

--- src/commands/bot-info.js
+++ src/commands/bot-info.js
@@ -7,9 +7,7 @@ module.exports = {
     name: "bot-info",
     aliases: ["info", "botinfo"],
     run: async (client, message, args) => {
-        var msg = await message.channel.send("Getting information...");
-
-        var osType = await os.type();
+        let osType = await os.type();
         let space = 0;
 
         if (osType === "Darwin") osType = "macOS";
@@ -25,65 +23,58 @@ module.exports = {
             title: "Bot Information",
             fields: [
                 {
-                    name: "❯ Bot Owner",
+                    name: "Bot Owner",
                     value: client.users.cache.get("716761186812821604"),
                     inline: true
                 },
                 {
-                    name: "❯ Uptime",
+                    name: "Uptime",
                     value: moment
                         .duration(client.uptime)
-                        .format("D [Day], H [Hour], m [Minutes], s [Seconds]"),
+                        .format("d [Days], h [Hours], m [Minutes], s [Seconds]"),
                     inline: true
                 },
                 {
-                    name: "❯ Server OS",
+                    name: "Server OS",
                     value: `${osType}`,
                     inline: true
                 },
                 {
-                    name: "❯ Memory",
+                    name: "Memory",
                     value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
                     inline: true
                 },
                 {
-                    name: "❯ Storage Used",
+                    name: "Storage Used",
                     value: `${space} KB`,
                     inline: true
                 },
                 {
-                    name: "❯ CPU Model",
-                    value: `• ${os.cpus().map(i => `${i.model}`)[0]} `,
+                    name: "CPU Model",
+                    value: `${os.cpus().map(i => `${i.model}`)[0]}`,
                     inline: true
                 },
                 {
-                    name: "❯ CPU Usage",
-                    value: `• ${Math.round(require("os").loadavg()[0] * 100) / 100}%`,
+                    name: "CPU Usage",
+                    value: `${Math.round(require("os").loadavg()[0] * 100) / 100}%`,
                     inline: true
                 },
                 {
-                    name: " ❯ General Information",
-                    value: `             
-• Servers: ${client.guilds.cache.size}                   
-• Channels: ${client.channels.cache.size}                   
-• Users: ${client.guilds.cache
-                            .reduce((a, b) => a + b.memberCount, 0)
-                            .toLocaleString()}
-                    `,
+                    name: "General Information",
+                    value: `Servers: ${client.guilds.cache.size}\nChannels: ${client.channels.cache.size}\nUsers: ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}`,
                     inline: true
                 },
                 {
-                    name: "❯ Versions",
-                    value: `
-           • Discord.JS: v${Discord.version}
-           • Node.JS: ${process.version}
-                    `,
+                    name: "Version Information",
+                    value: `Discord.JS: v${Discord.version}\nNode.JS: ${process.version}`,
                     inline: true
                 }
             ],
-            thumbnail: { url: client.user.displayAvatarURL() }
+            thumbnail: {
+                url: client.user.displayAvatarURL()
+            }
         };
 
-        msg.edit("", { embed });
+        return message.channel.send({ embed });
     }
 }
