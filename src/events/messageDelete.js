e1b90b9e32649a11f0ec9d2573ef5079ae5e56e3
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-04-18 08:15:15 -0400

    message delete logs

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -1,6 +1,36 @@
+const Discord = require("discord.js");
+const webhook = {
+    token: "XPRN4_kSCSILR2YQmRymQeWvPfhCkUqOnyLYm2ahMH50ghGVRpklwwDyu1cnIwVTQO8y",
+    id: "826519427121807361"
+};
+
 module.exports = {
     name: "messageDelete",
     run: async (client, message) => {
+        let embed = new Discord.MessageEmbed()
+            .setTitle("Message Deleted")
+            .addFields(
+                {
+                    name: "Content",
+                    value: message.content,
+                    inline: true
+                },
+                {
+                    name: "Channel",
+                    value: message.channel,
+                    inline: true
+                },
+                {
+                    name: "Author",
+                    value: message.author,
+                    inline: true
+                }
+            )
+
+        const hook = new Discord.WebhookClient(webhook.id, webhook.token);
+
+        hook.send(embed);
+
         let snipes = client.snipes;
 
         snipes.set(`${message.guild.id}:${message.channel.id}`, {
