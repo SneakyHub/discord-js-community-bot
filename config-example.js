c43fef3d771b8abf61f205b4be3317fcb95742c1
Sneaky <78713939+SneakyHub@users.noreply.github.com>
2021-04-18 10:55:37 +0100

    Update config-example.js
    added more documentation

--- config-example.js
+++ config-example.js
@@ -1,8 +1,8 @@
 module.exports = {
     // Discord bot
     // rename this file to "config.js"
-    token: "your token here",
-    prefix: "prefix here",
+    token: "your token here",     // Add your bot token
+    prefix: "prefix here",    // Our default prefix is "-"
     eval_ids: [],
-    logs_channel: ""
+    logs_channel: ""     // add your logs channel here
 }
