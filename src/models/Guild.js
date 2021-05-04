f4991aa1558b2ca0e24d4e40c09eb7f736a445dc
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-05-04 08:23:52 -0400

    mongodb

---
+++ src/models/Guild.js
@@ -0,0 +1,16 @@
+const Mongoose = require("mongoose")
+
+const GuildSchema = new Mongoose.Schema({
+    guildId: {
+        required: true,
+        type: String
+    },
+    guildName: {
+        required: false,
+        type: String
+    },
+    prefix: {
+        required: false,
+        type: String
+    }
+})
