0441389fbddc6935e314f6c316c6faa799a799ee
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-05-04 22:46:19 -0400

    suggestion model

---
+++ src/models/Suggestion.js
@@ -0,0 +1,22 @@
+const Mongoose = require("mongoose")
+
+const SuggestionSchema = new Mongoose.Schema({
+    suggestionId: {
+        required: true,
+        type: String
+    },
+    guildId: {
+        required: true,
+        type: String
+    },
+    content: {
+        required: false,
+        type: String
+    },
+    repliedTo: {
+        required: false,
+        type: Boolean
+    }
+})
+
+module.exports = Mongoose.model("suggestion", SuggestionSchema)
