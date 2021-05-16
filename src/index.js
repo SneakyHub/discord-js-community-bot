d420f14d04a6f406cad2b2ad59816e706b65ddbc
carlromulo <77764864+carlromulo@users.noreply.github.com>
2021-05-17 01:53:22 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const config = require("../config");
 const LoadCommands = require("./utils/LoadCommands");
 const LoadEvents = require("./utils/LoadEvents");
 
+client.on("messageUpdate", async(m, nm) => { //m = old mesage, nm = new message
+  if (m.author.bot) return; //bot can't run commands
+  if (!client.editSnipes.get(m.channel.id)) client.editSnipes.set(m.channel.id, []); //if there are no snipes, turn it to an empty array
+  const editSnipes = client.editSnipes.get(m.channel.id); //the array of edit snipes in the current channel
+  const obj = { m, nm, editedAt: Date.now() }; // an object containing both the old and new messages
+  client.editSnipes.set(m.channel.id, editSnipes.concat([obj])); //save it to client.editSnipes
+});
+
 client.commands = new Discord.Collection();
 client.snipes = new Map();
 client.antispam = new Map();
