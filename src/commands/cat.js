011191689fab6e37c59ed2f5a0a922e94f56d8f7
TheUsual10k <82567115+TheUsual10k@users.noreply.github.com>
2021-05-20 10:05:06 +0500

    cat.js

---
+++ src/commands/cat.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "cat",
+    category: "animals",
+    run: async(client, message, args) => {
+        const url = "https://some-random-api.ml/img/cat";
+        const facts = "https://some-random-api.ml/facts/cat"
+
+        let image, response;
+        let fact, responses;
+        try {
+            response = await axios.get(url);
+            image = response.data;
+
+            responses = await axios.get(facts)
+            fact = responses.data
+
+        } catch (e) {
+            return message.channel.send(`An error occured, please try again!`)
+        }
+
+        const embed = new MessageEmbed()
+            .setTitle(`Random Cat Image and Fact`)
+            .setColor(`#f3f3f3`)
+            .setDescription(fact.fact)
+            .setImage(image.link)
+
+            message.channel.send(embed).catch(err => console.log(err));
+    }
+}
