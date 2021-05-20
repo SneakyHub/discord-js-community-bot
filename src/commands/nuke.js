df2b2a0acf863fc0a02d2f841c7c6e39b9af924e
TheUsual10k <82567115+TheUsual10k@users.noreply.github.com>
2021-05-20 10:19:27 +0500

    Nooke ree
    Nuke Command to Delete and replace channels , changes are recommended

---
+++ src/commands/nuke.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require('discord.js')
+module.exports = {
+    name: "nuke",
+    description: "Nukes a given channel",
+    authorPermission: "ADMINISTRATOR",
+    callback: async({message, args}) => {
+        let reason = args.join(" ") || "No Reason"
+        if(!message.channel.deletable) {
+            return message.reply("This channel cannot be nuked!")
+        }
+        let newchannel = await message.channel.clone()
+        await message.channel.delete()
+        let embed = new MessageEmbed()
+        .setTitle("Channel Nuked")
+        .setDescription(reason)
+        .setColor('RANDOM')
+        .setImage('https://media0.giphy.com/media/oe33xf3B50fsc/200.gif')
+        .setTimestamp()
+        await newchannel.send(embed)
+    }
+}
