ba68374e20c40efd5c6ffbc6dcb6af36030d4460
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-05-21 14:05:52 -0400

    disabling this since the support bot is up and running

--- src/events/message/keywords.js
+++ src/events/message/keywords.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js")
 
 module.exports = {
     name: "message",
+    disabled: true,
     run: async (client, message) => {
         if (message.channel.id != "828253375387795486") return
         if (message.author.bot) return
