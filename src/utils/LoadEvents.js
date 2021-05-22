c9b2a1f864591c99a5b679821dcc73cc57822309
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-05-22 01:13:36 -0400

    fix pls

--- src/utils/LoadEvents.js
+++ src/utils/LoadEvents.js
@@ -2,12 +2,12 @@ const path = require("path");
 const fs = require("fs");
 const LoadEvents = require("./LoadEvents");
 
-module.exports = async (client, dir = "") => {
+module.exports.run = async (client, dir = "") => {
     const filePath = path.join(__dirname, dir);
     const files = await fs.readdirSync(filePath);
     for (const file of files) {
         const stat = await fs.lstatSync(path.join(filePath, file));
-        if (stat.isDirectory()) LoadEvents(client, path.join(dir, file));
+        if (stat.isDirectory()) LoadEvents.run(client, path.join(dir, file));
         if (file.endsWith('.js')) {
             const event = require(path.join(filePath, file));
             if (event.disabled == true) {
