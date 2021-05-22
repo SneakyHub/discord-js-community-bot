99213a3ca05a07f5a5c0fc6affe38cc36c3c45db
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-05-22 01:28:54 -0400

    update

--- src/commands/bot_admins/restartBot.js
+++ src/commands/bot_admins/restartBot.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "restart-bot",
     aliases: [],
     run: async (client, message, args) => {
-        if (message.author.id != "716761186812821604") return
+        if (message.author.id != "716761186812821604") return message.reply("only my owner can use this.")
 
         message.channel.send("Restarting the bot...")
 
