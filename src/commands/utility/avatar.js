98c7d778914d34408b0fbb729916b6eed58d3a12
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-05-22 01:49:21 -0400

    reeee

--- src/commands/utility/avatar.js
+++ src/commands/utility/avatar.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "avatar",
     aliases: ["av"],
     description: "Displays someone's avatar!",
-    run: async (message, client, args) => {
+    run: async (client, message, args) => {
         const user = message.mentions.users.first() || message.author || client.users.cache.get(u => u.id === args[0])
         const avatar = user.displayAvatarURL({ size: 4096, dynamic: true })
         let embed = new MessageEmbed()
