d38e4ebbbacd6bb72c44fdc9ae58b28e7794b657
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-05-22 02:04:24 -0400

    forgot something

--- src/events/guild/guildMemberAdd.js
+++ src/events/guild/guildMemberAdd.js
@@ -31,6 +31,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
             .setTitle("New Member!")
             .setColor("RANDOM")
+            .setThumbnail(member.displayAvatarURL())
             .setDescription(`${member} welcome to ${member.guild.name}, head over to <#825797019103789056> to get started with hosting! If you need help feel free to ask for it in <#828253375387795486>!`)
             .setFooter("SneakyHub")
 
