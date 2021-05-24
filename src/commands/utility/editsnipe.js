bd9099a9c04fc58dd1e6c6830ebad05c6e89fd7b
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-05-24 06:48:13 -0400

    attempt to fix editsnipe again

--- src/commands/utility/editsnipe.js
+++ src/commands/utility/editsnipe.js
@@ -18,12 +18,12 @@ module.exports = {
         const editSnipe = (editSnipes.reverse())[num - 1]; //the editSnipe user requested, may be a bit complicating for you
         if (!editSnipe) return message.channel.send(`No edited message with number ${num} found`); //send a message if no edit-snipe indexed by what the user ent
         const e = new Discord.MessageEmbed()
-            .setAuthor(`${editSnipe.m.author.tag} (${editSnipe.m.author.id})`, editSnipe.m.author.avatarURL({ dynamic: 1 }))
-            .setDescription(editSnipe.m.content || "null")
+            .setAuthor(`${editSnipe.oldMessage.author.tag} (${editSnipe.oldMessage.author.id})`, editSnipe.oldMessage.author.avatarURL({ dynamic: 1 }))
+            .setDescription(editSnipe.oldMessage.content || "null")
             .setColor("RANDOM")
             .setFooter(`${num}/${editSnipes.length}`)
             .setTimestamp(editSnipe.editedAt);
-        if (editSnipe.m.attachments.first()) e.setImage(editSnipe.m.attachments.first().url); //the user cant edit or add attachments onto an edited message, so this works
+        if (editSnipe.oldMessage.attachments.first()) e.setImage(editSnipe.oldMessage.attachments.first().url); //the user cant edit or add attachments onto an edited message, so this works
         editSnipes.reverse();
 
         message.channel.send({ embed: e });
