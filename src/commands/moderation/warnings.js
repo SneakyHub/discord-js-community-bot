a5697e87b270e0fac21faaa8026647354079b17f
TheUsual10k <82567115+TheUsual10k@users.noreply.github.com>
2021-05-24 10:21:11 +0500

    Create warnings.js

---
+++ src/commands/moderation/warnings.js
@@ -0,0 +1,22 @@
+const db = require('quick.db');
+
+module.exports = {
+    name: "warnings",
+    description: "Check a users warnings",
+
+    async run (client, message, args){
+        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
+
+
+        let warnings = await db.get(`warnings_${message.guild.id}_${user.id}`);
+
+        if(warnings === null) warnings = 0;
+
+        const embed = Discord.MessageEmbed()
+        .setTitle('Warnings')
+        .setDescription(`**${user.username}** has **${warnings}** warning(s)`)
+        .setFooter(message.author.username,  message.author.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()
+        .setColor('RANDOM');
+    }
+}
