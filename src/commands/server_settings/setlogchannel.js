91f379e845cd4911a7ea91f8c3fbbb104c2122b5
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-05-28 10:10:59 -0400

    update setlogchannel.js

--- src/commands/server_settings/setlogchannel.js
+++ src/commands/server_settings/setlogchannel.js
@@ -7,16 +7,8 @@ module.exports = {
     description: "Sets the channel where the bot will log events such as mutes.",
     category: "Server-Settings",
     run: async (client, message, args) => {
-        if (!message.member.hasPermission("ADMINISTRATOR")) { // admin cuz testing
-            const errorEmbed = new Discord.MessageEmbed()
-                .setAuthor(message.author.tag)
-                .setTitle(":x: Error!")
-                .setColor("RED")
-                .setThumbnail(message.guild.iconURL())
-                .setDescription("You have insufficient permissions to run this command.")
-                .setFooter("Powered by SneakyHub")
-
-            return message.channel.send(errorEmbed)
+        if (!message.member.hasPermission("MANAGE_GUILD")) {
+            client.sendErrorEmbed(message, "You have insufficient permissions to run this command.")
         }
     }
 }
