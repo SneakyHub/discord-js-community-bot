88acb885785a67fcf80a56239c15685565899adf
micro <67523190+Microwave-6443@users.noreply.github.com>
2021-05-28 09:57:32 -0400

    server settings cmd category

---
+++ src/commands/server_settings/setlogchannel.js
@@ -0,0 +1,22 @@
+const Guild = require("../../models/Guild")
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "setlogchannel",
+    aliases: ["set-log-channel"],
+    description: "Sets the channel where the bot will log events such as mutes.",
+    category: "Server-Settings",
+    run: async (client, message, args) => {
+        if (!message.member.hasPermission("MANAGE_GUILD")) {
+            const errorEmbed = new Discord.MessageEmbed()
+                .setAuthor(message.author.tag)
+                .setTitle(":x: Error!")
+                .setColor("RED")
+                .setThumbnail(message.guild.iconURL())
+                .setDescription("You have insufficient permissions to run this command.")
+                .setFooter("Powered by SneakyHub")
+
+            return message.channel.send(errorEmbed)
+        }
+    }
+}
